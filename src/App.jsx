189363a83b0f710304b6f6f2227697244d413a3a
Muonaka Anthony <Antonynans@gmail.com>
2023-12-22 12:33:14 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 
-import What_we_do from "./components/WHat_we_do";
+import What_we_do from "./components/What_we_do";
 import What_our_clients_say from "./components/What_our_clients_say";
 
 const App = () => {
